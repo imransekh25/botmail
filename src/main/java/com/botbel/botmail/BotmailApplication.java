1aca421a37a566a6c3ca8fa8ad6d483622413ed3
imran <imran>
2023-10-11 22:50:45 +0530

    New Project For Email Marketing

---
+++ src/main/java/com/botbel/botmail/BotmailApplication.java
@@ -0,0 +1,13 @@
+package com.botbel.botmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botbel/botmail/BotmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botbel.botmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
